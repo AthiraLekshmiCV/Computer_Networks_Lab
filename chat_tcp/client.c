20347c88abab80b08f45a84b84bdc17c82a9467d
AthiraLekshmiCV <athiralek@gmail.com>
2015-04-30 19:34:00 +0530

    A simple char program

---
+++ chat_tcp/client.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<netinet/tcp.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<ctype.h>
+#include<netdb.h>
+int main(){
+
+	struct sockaddr_in server_addr;
+	char smsg[100],rmsg[100];
+	int c_sockfd;
+	
+	c_sockfd=socket(AF_INET,SOCK_STREAM,0);	//creates a socket
+	
+	bzero((char*)&server_addr,sizeof(server_addr));
+	
+	server_addr.sin_family=AF_INET;
+	inet_aton("127.0.0.1",&server_addr.sin_addr);
+	server_addr.sin_port=htons(2345);
+	
+	connect(c_sockfd,(struct sockaddr*)&server_addr,sizeof(server_addr));   //Connect the socket to the address of the server
+	
+	printf("\n");
+	
+	//send and recieve data.
+	//type "bye" to exit
+	
+	for(;;){
+	    
+		printf("Client>>");
+		scanf("%s",smsg);
+	
+		
+		
+		send(c_sockfd,smsg,100,0);
+
+		if((strcmp(smsg,"bye"))==0){
+		 
+		  exit(0); 
+		}
+		recv(c_sockfd,rmsg,100,0);
+		
+		printf("\nServer>>%s\n",rmsg);
+		printf("\n");
+	}
+	
+	close(c_sockfd);	
+
+
+	return 0;
+}
---
+++ chat_tcp/server.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<netinet/in.h>
+#include<netinet/tcp.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<ctype.h>
+
+int main(){
+	  
+    	struct sockaddr_in server_addr,client_addr;
+	char rmsg[100],smsg[100];
+	int c_sockfd,s_sockfd;
+	
+	int clientlen=sizeof(client_addr);
+	s_sockfd=socket(AF_INET,SOCK_STREAM,0);	//creates a socket
+	
+	bzero((char*)&server_addr,sizeof(server_addr));
+	
+	server_addr.sin_family=AF_INET;
+	inet_aton("127.0.0.1",&server_addr.sin_addr);
+	server_addr.sin_port=htons(2345);
+	
+	bind(s_sockfd,(struct sockaddr*)&server_addr,sizeof(server_addr));	//bind the socket to an address
+	
+	listen(s_sockfd,5);	//listen for connections	
+	
+	c_sockfd=accept(s_sockfd,(struct sockaddr*)&client_addr,&clientlen);	//accept a connections
+	
+	//send and recieve data
+	//stop when client says bye
+	for(;;){
+	
+		recv(c_sockfd,rmsg,100,0);
+		
+		printf("\nClient>>%s\n",rmsg);
+		if((strcmp(rmsg,"bye"))==0){
+			
+			exit(0);		
+		}
+		printf("\nServer>>");
+		scanf("%s",smsg);
+	
+		send(c_sockfd,smsg,100,0);
+	
+	}
+	
+	close(s_sockfd);
+	close(c_sockfd);
+	
+	return 0;
+}
